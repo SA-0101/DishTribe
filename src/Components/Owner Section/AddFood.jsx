c59c1bbfe20ba3a45a18786b3614084fbe36af08
shaan Aslam <shaanaslam0101@gmail.com>
2025-06-25 09:13:44 +0500

    update

---
+++ src/Components/Owner Section/AddFood.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddFood() {
+  return (
+    <div>
+        Add New Food
+    </div>
+  )
+}
+
+export default AddFood
---
+++ src/Components/Owner Section/OrderManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OrderManagement() {
+  return (
+    <div>
+            Order Management
+    </div>
+  )
+}
+
+export default OrderManagement
