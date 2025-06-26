c2c53ec88b64d95e4cedc0b99b4b5861d28b3b36
shaan Aslam <shaanaslam0101@gmail.com>
2025-06-26 16:09:31 +0500

    update

--- src/Components/LandingPage/Landing.jsx
+++ src/Components/LandingPage/Landing.jsx
@@ -1,11 +1,13 @@
 import Navbar from './Navbar'
 import LandingBanners from '../LandingPage/LandingBanners'
+import NearestRestourents from './NearestRestourents'
 
 function Landing() {
   return (
     <div>
       <Navbar/>
       <LandingBanners/>
+      <NearestRestourents/>
     </div>
   )
 }
--- src/Components/LandingPage/NearestRestourents.jsx
+++ src/Components/LandingPage/NearestRestourents.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+ 
 function NearestRestourents() {
   return (
-    <div>
-        Nearest Resturent
+    <div className='flex flex-col gap-10 px-16 py-12'>
+        <div className='flex flex-col items-center gap-4'>
+          <h1 className='text-2xl font-semibold'>Nearby Restaurants</h1>
+          <h1>Discover delicious food within 1km of your location</h1>
+        </div>
+        <div className='bg-blue-50 rounded-lg'>
+          <div className='px-4 py-4 rounded-tr-lg rounded-tl-lg bg-yellow-100 w-full h-full'>
+            <h1 className='font-semibold'>Map View</h1>
+            <h1>Your location and nearby restaurants</h1>
+          </div>
+          <div>
+            Reall MAP
+          </div>
+        </div>
+        <div className='flex flex-col gap-4 bg-green-100'>
+          <h1>Nearest Restaurants</h1>
+          <div className='flex gap-5'>
+            <h1>Restaurant 1</h1>
+            <h1>Restaurant 2</h1>
+          </div>
+        </div>
     </div>
   )
 }
