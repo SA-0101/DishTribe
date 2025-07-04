e685298075edefa89ca2d8b8462337397831653e
shaan Aslam <shaanaslam0101@gmail.com>
2025-07-04 16:46:54 +0500

    upfate

--- src/Components/Admin Section/AdminDashboard.jsx
+++ src/Components/Admin Section/AdminDashboard.jsx
@@ -48,7 +48,7 @@ function AdminDashboard() {
             </div>
           </NavLink>
 
-          <NavLink to="Setting" className={({ isActive }) => `group w-full rounded-lg transition ${isActive ? 'bg-blue-100 text-blue-700' : 'hover:bg-gray-100 text-gray-700'}`}>
+          <NavLink to="OrderManagement" className={({ isActive }) => `group w-full rounded-lg transition ${isActive ? 'bg-blue-100 text-blue-700' : 'hover:bg-gray-100 text-gray-700'}`}>
             <div className='flex items-center gap-3 px-4 py-2 font-medium'>
               ⚙️ <span>Setting</span>
             </div>
